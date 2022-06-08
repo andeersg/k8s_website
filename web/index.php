e45ff0bd3c41c54b1a9f8c1cbfb5132495aac582
Anders Grendstadbakk <anders.2205@gmail.com>
2022-06-08 14:59:06 +0200

    Trigger change

--- web/index.php
+++ web/index.php
@@ -7,6 +7,6 @@
     <h1>This website is hosted in the ☁</h1>
 
     <p>The hash salt is: <?php getenv('DRUPAL_HASH_SALT'); ?>!</p>
-    <p>Testing stuff!!</p>
+    <p>Testing stuff!!!</p>
   </body>
 </html>
