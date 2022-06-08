21b4dcf5bc71faffeb952de5ab97127d1938cf52
Anders Grendstadbakk <anders.2205@gmail.com>
2022-06-08 15:32:53 +0200

    Trigger change

--- web/index.php
+++ web/index.php
@@ -9,6 +9,7 @@ $env = getenv();
   </head>
   <body>
     <h1>This website is hosted in the ☁</h1>
+    <p>Docker + Kubernetes</p>
 
     <p>Environment variables:</p>
     <pre><?php echo json_encode($env, JSON_PRETTY_PRINT) ?></pre>
