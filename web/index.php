6fb1bf66d486ddd339aede161aa314fd49c03437
Anders Grendstadbakk <anders.2205@gmail.com>
2022-06-09 08:38:07 +0200

    Trigger

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ $env = getenv();
   </head>
   <body>
     <h1>This website is hosted in the ☁</h1>
-    <p>Docker + Kubernetes</p>
+    <p>Docker + Kubernetes + Rancher</p>
 
     <p>Environment variables:</p>
     <pre><?php echo json_encode($env, JSON_PRETTY_PRINT) ?></pre>
